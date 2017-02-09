98094d6191350dc795ba446decf5a419366cdab6
pertsev <peretsmobil@gmail.com>
2017-02-09 12:19:41 +1000

    There are false positive testes for jsloose context. Not all passed, but it is not DOMSanitizer's problem.

--- test/runLoose.js
+++ test/runLoose.js
@@ -5,7 +5,7 @@ var url = 'http:/localhost:9000/tmp/samples/page.html';
 casper.test.begin('False Positive tests for JSLoose', function suite(test) {
     casper.start();
 
-    var suite = require('specLoose/jsl.spec.js');
+    var suite = require('specLoose/jsl.spec.small.js');
 
     suite.tests.forEach(function(ctest) {
         casper.thenOpen(url + '#' + ctest, function() {
